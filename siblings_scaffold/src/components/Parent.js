d3772d6b1b9b86c169234ebf1a2d0ac2cee87dd8
Sanchi Wahane <wahane.sanchi02@gmail.com>
2026-01-04 20:17:36 +0530

    sibling component

---
+++ siblings_scaffold/src/components/Parent.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+// SiblingA Component
+class SiblingA extends Component {
+  handleChange = (event) => {
+    // TODO: Implement handleChange
+    this.props.setName(event.target.value)
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>Sibling A</h2>
+        <input type="text" 
+        placeholder="Enter name" 
+        value={this.props.name}
+        onChange={this.handleChange}
+        />
+      </div>
+    );
+  }
+}
+
+// SiblingB Component
+class SiblingB extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Sibling B</h2>
+        <p>Name: {this.props.name}</p>
+      </div>
+    );
+  }
+}
+
+// Parent Component
+export default class Parent extends Component {
+  state = { /* TODO: Initialize state */
+           name:""
+   };
+
+  setName = (newName) => {
+    // TODO: Implement setName
+    this.setState({name:newName});
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Parent Component</h1>
+        {/* TODO: Render SiblingA and SiblingB components */}
+         <SiblingA name={this.state.name} setName={this.setName} />
+        <SiblingB name={this.state.name} />
+      </div>
+    );
+  }
+}
